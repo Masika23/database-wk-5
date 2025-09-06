69baa6a12b4a924990a55a793812c02de06ec2fa
Masika23 <masikabayah25@gmail.com>
2025-09-06 13:47:33 +0300

    Add files via upload

---
+++ answers.sql
@@ -0,0 +1,11 @@
+QUERY 1
+DROP INDEX IdxPhone ON customers;
+
+QUERY 2
+CREATE USER 'bob'@'localhost' IDENTIFIED BY '$scu3r3!';
+
+QUERY 3
+GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
+
+QUERY 4
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P''55!23';
